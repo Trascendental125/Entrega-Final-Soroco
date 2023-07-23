4ca1eddfc40a937e0aa9dda96252508c6a66d601
Trascendental125 <asoroco@gmail.com>
2023-07-23 12:55:59 -0300

    Estructura de archivos

--- project/apps/Home/views.py
+++ project/apps/Home/views.py
@@ -1,3 +1,15 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.http import HttpResponse
+from django.http.request import HttpRequest
 from django.shortcuts import render
 
+from . import forms
+
 # Create your views here.
+
+def home(request):
+    return render(request, "Home/index.html")
+
